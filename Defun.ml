34cea3cdf150abe085a419d215eefa0e16ba0a15
Ekdohibs <nathanael.courant@laposte.net>
2017-12-16 15:14:14 +0100

    Use mutable state for Defun

--- Defun.ml
+++ Defun.ml
@@ -8,21 +8,16 @@ module IMap = Map.Make(struct type t = int let compare = compare end)
 let gen_id =
   let c = ref 0 in fun () -> (incr c; !c - 1)
 
-type defun_state = T.function_declaration list *
-                   ((Atom.atom * Atom.atom list * T.branch list) IMap.t)
+type defun_state = {
+  mutable extracted_functions : T.function_declaration list ;
+  mutable apply_defs : (Atom.atom * Atom.atom list * T.branch list) IMap.t ;
+}
 
 let ( <|> ) a b =
   let rec aux a b r =
     if b <= a then r
     else aux a (b - 1) ((b - 1) :: r)
   in aux a b []
-
-let get_apply apply arity =
-  try IMap.find arity apply, apply
-  with Not_found ->
-    let v = Atom.fresh ("defun_apply_" ^ (string_of_int arity) ^ "_") in
-    let args = List.map (fun _ -> Atom.fresh "defun_apply_var") (0 <|> arity) in
-    (v, args, []), IMap.add arity (v, args, []) apply
 
 let rec simpl (t : S.term) : S.term =
   match t with
@@ -37,27 +32,30 @@ let rec simpl (t : S.term) : S.term =
       | _ -> S.LetBlo (f, S.Lam (self, args, body), t)
     end
 
-let rec defun (t : S.term) (st : defun_state) : (defun_state * T.term) =
+let get_apply_def (st : defun_state) (arity : int) =
+  try
+    IMap.find arity st.apply_defs
+  with Not_found ->
+    let v = Atom.fresh ("defun_apply_" ^ (string_of_int arity) ^ "_") in
+    let args = List.map (fun _ -> Atom.fresh "defun_apply_var") (0 <|> arity) in
+    st.apply_defs <- IMap.add arity (v, args, []) st.apply_defs;
+    (v, args, [])
+
+let rec defun (t : S.term) (st : defun_state) : T.term =
   match t with
-  | S.Exit -> (st, T.Exit)
+  | S.Exit -> T.Exit
   | S.TailCall (f, args) ->
-    let fcts, apply = st in
     let arity = List.length args in
-    let (app, _, _), apply = get_apply apply arity in
-    ((fcts, apply), T.TailCall (app, (f :: args)))
-  | S.Print (v, t) ->
-    let st, nt = defun t st in
-    (st, T.Print (v, nt))
-  | S.LetVal (x, v, t) ->
-    let st, nt = defun t st in
-    (st, T.LetVal (x, v, nt))
+    let app, _, _ = get_apply_def st arity in
+    T.TailCall (app, (f :: args))
+  | S.Print (v, t) -> T.Print (v, defun t st)
+  | S.LetVal (x, v, t) -> T.LetVal (x, v, defun t st)
   | S.LetBlo (f, (S.Lam (self, args, body) as blo), t) ->
     let fv = Atom.Set.elements (S.fv_block blo) in
     let fv1 = List.map Atom.copy fv in
     let ren = List.fold_left2 (fun m u v -> Atom.Map.add u v m) Atom.Map.empty fv fv1 in
-    let st, nbody = defun (S.rename_term ren body) st in
-    let st, nt = defun t st in
-    let fcts, apply = st in
+    let nbody = defun (S.rename_term ren body) st in
+    let nt = defun t st in
     let tag = gen_id () in
     let fname = match self with
       | S.NoSelf -> Atom.fresh "defun_lambda"
@@ -69,17 +67,18 @@ let rec defun (t : S.term) (st : defun_state) : (defun_state * T.term) =
     in
     let fdef = T.Fun (fname, args @ fv1, nbody) in
     let arity = List.length args in
-    let (app, aargs, branches), apply = get_apply apply arity in
+    let app, aargs, branches = get_apply_def st arity in
     let fv2 = List.map Atom.copy fv in
     let branch = T.Branch (tag, fv2, T.TailCall (fname, T.vvars (aargs @ fv2))) in
-    let apply = IMap.add arity (app, aargs, branch :: branches) apply in
-    let fcts = fdef :: fcts in
-    ((fcts, apply), T.LetBlo (f, T.Con (tag, T.vvars fv), nt))
+    st.apply_defs <- IMap.add arity (app, aargs, branch :: branches) st.apply_defs;
+    st.extracted_functions <- fdef :: st.extracted_functions;
+    T.LetBlo (f, T.Con (tag, T.vvars fv), nt)
 
 let defun_term (t : S.term) : T.program =
-  let (fcts, apply), nt = defun (simpl t) ([], IMap.empty) in
+  let st = { extracted_functions = []; apply_defs = IMap.empty } in
+  let nt = defun (simpl t) st in
   let applies = List.map (fun (_, (name, args, branches)) ->
       let f = Atom.fresh "apply_f" in
       T.Fun (name, f :: args, T.Swi (T.vvar f, branches))
-    ) (IMap.bindings apply) in
-  T.Prog (applies @ fcts, nt)
+    ) (IMap.bindings st.apply_defs) in
+  T.Prog (applies @ st.extracted_functions, nt)
