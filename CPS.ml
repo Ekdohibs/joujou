aa09f293093f9c3c55f6c4ba7a8a037b842a237d
Ekdohibs <nathanael.courant@laposte.net>
2017-12-16 14:02:26 +0100

    Add CPS and Defun

--- CPS.ml
+++ CPS.ml
@@ -3,5 +3,41 @@ module S = Lambda
 (* The target calculus. *)
 module T = Tail
 
+let lambda_let (lam : T.block) (body : T.value -> T.term) : T.term =
+  let f = Atom.fresh "cps_lambda_let" in
+  T.LetBlo (f, lam, body (T.vvar f))
+
+let rec cps (t : S.term) (k : T.value) : T.term =
+  match t with
+  | S.Var v -> T.TailCall (k, [T.vvar v])
+  | S.Lam (self, var, body) ->
+    let cont = Atom.fresh "cps_cont" in
+    lambda_let (T.Lam (self, [var; cont], cps body (T.vvar cont)))
+      (fun f -> T.TailCall (k, [f]))
+  | S.App (t1, t2) ->
+    let appl = Atom.fresh "cps_appl" in
+    let appr = Atom.fresh "cps_appr" in
+    let w =
+      lambda_let (T.Lam (T.NoSelf, [appr],
+                         T.TailCall (T.vvar appl, [T.vvar appr; k])))
+        (cps t2) in
+    lambda_let (T.Lam (T.NoSelf, [appl], w)) (cps t1)
+  | S.Lit n -> T.TailCall (k, [T.VLit n])
+  | S.BinOp (t1, op, t2) ->
+    let bl = Atom.fresh "cps_bl" in
+    let br = Atom.fresh "cps_br" in
+    let w =
+      lambda_let (T.Lam (T.NoSelf, [br],
+                         T.TailCall (k, [T.VBinOp (T.vvar bl, op, T.vvar br)])))
+        (cps t2) in
+    lambda_let (T.Lam (T.NoSelf, [bl], w)) (cps t1)
+  | S.Print t ->
+    let pr = Atom.fresh "cps_pr" in
+    lambda_let (T.Lam (T.NoSelf, [pr],
+                       T.Print (T.vvar pr, T.TailCall (k, [T.vvar pr]))))
+      (cps t)
+  | S.Let (x, t1, t2) ->
+    lambda_let (T.Lam (T.NoSelf, [x], cps t2 k)) (cps t1)
+
 let cps_term (t : S.term) : T.term =
-  assert false
+  lambda_let (T.Lam (T.NoSelf, [Atom.fresh "cps_result"], T.Exit)) (cps t)
--- Defun.ml
+++ Defun.ml
@@ -3,5 +3,68 @@ module S = Tail
 (* The target calculus. *)
 module T = Top
 
+module IMap = Map.Make(struct type t = int let compare = compare end)
+
+let gen_id =
+  let c = ref 0 in fun () -> (incr c; !c - 1)
+
+type defun_state = T.function_declaration list *
+                   ((Atom.atom * Atom.atom list * T.branch list) IMap.t)
+
+let ( <|> ) a b =
+  let rec aux a b r =
+    if b <= a then r
+    else aux a (b - 1) ((b - 1) :: r)
+  in aux a b []
+
+let get_apply apply arity =
+  try IMap.find arity apply, apply
+  with Not_found ->
+    let v = Atom.fresh ("defun_apply_" ^ (string_of_int arity) ^ "_") in
+    let args = List.map (fun _ -> Atom.fresh "defun_apply_var") (0 <|> arity) in
+    (v, args, []), IMap.add arity (v, args, []) apply
+
+let rec defun (t : S.term) (st : defun_state) : (defun_state * T.term) =
+  match t with
+  | S.Exit -> (st, T.Exit)
+  | S.TailCall (f, args) ->
+    let fcts, apply = st in
+    let arity = List.length args in
+    let (app, _, _), apply = get_apply apply arity in
+    ((fcts, apply), T.TailCall (app, (f :: args)))
+  | S.Print (v, t) ->
+    let st, nt = defun t st in
+    (st, T.Print (v, nt))
+  | S.LetVal (x, v, t) ->
+    let st, nt = defun t st in
+    (st, T.LetVal (x, v, nt))
+  | S.LetBlo (f, (S.Lam (self, args, body) as blo), t) ->
+    let st, nbody = defun body st in
+    let st, nt = defun t st in
+    let fcts, apply = st in
+    let fv = Atom.Set.elements (S.fv_block blo) in
+    let tag = gen_id () in
+    let fname = match self with
+      | S.NoSelf -> Atom.fresh "defun_lambda"
+      | S.Self f -> Atom.copy f
+    in
+    let nbody = match self with
+      | S.NoSelf -> nbody
+      | S.Self f -> T.LetBlo (f, T.Con (tag, T.vvars fv), nbody)
+    in
+    let fdef = T.Fun (fname, args @ fv, nbody) in
+    let arity = List.length args in
+    let (app, aargs, branches), apply = get_apply apply arity in
+    let fvc = List.map Atom.copy fv in
+    let branch = T.Branch (tag, fvc, T.TailCall (fname, T.vvars (aargs @ fvc))) in
+    let apply = IMap.add arity (app, aargs, branch :: branches) apply in
+    let fcts = fdef :: fcts in
+    ((fcts, apply), T.LetBlo (f, T.Con (tag, T.vvars fv), nt))
+
 let defun_term (t : S.term) : T.program =
-  assert false
+  let (fcts, apply), nt = defun t ([], IMap.empty) in
+  let applies = List.map (fun (arity, (name, args, branches)) ->
+      let f = Atom.fresh "apply_f" in
+      T.Fun (name, f :: args, T.Swi (T.vvar f, branches))
+    ) (IMap.bindings apply) in
+  T.Prog (applies @ fcts, nt)
